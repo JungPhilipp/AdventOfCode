d6340af1277d4ff8ec42a21198bf5b41bd571e31
Philipp Jung <philipp.jung@mailbox.org>
2021-12-12 10:17:24 +0100

    2021: Problem 12 Part 1 + Part 2

--- src/problems_2021/problem12.rs
+++ src/problems_2021/problem12.rs
@@ -1,24 +1,82 @@
-use std::panic;
+use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
-use log::debug;
 
 use crate::util::parse::read_lines;
 
 pub static INPUT_PATH: &str = "src/problems_2021/problem12/input.txt";
 
-type Input = Vec<Vec<i32>>;
+type Edge = String;
+type Input = HashMap<Edge, HashSet<Edge>>;
+type Path = Vec<Edge>;
 
 pub fn parse_input(path_to_input: &str) -> Input {
-    vec![]
+    let input = read_lines(path_to_input)
+        .iter()
+        .map(|line| {
+            let connection: (String, String) = line
+                .split('-')
+                .map(|s| s.to_string())
+                .collect_tuple()
+                .unwrap();
+            [connection.clone(), (connection.1, connection.0)]
+        })
+        .flatten()
+        .collect_vec();
+
+    let mut adjacency_list = HashMap::<Edge, HashSet<Edge>>::new();
+    input.iter().for_each(|(start, end)| {
+        adjacency_list
+            .entry(start.clone())
+            .or_insert_with(HashSet::new)
+            .insert(end.clone());
+        adjacency_list
+            .entry(end.clone())
+            .or_insert_with(HashSet::new)
+            .insert(start.clone());
+    });
+    adjacency_list
 }
 
-pub fn solve_part1(input: &Input) -> usize {
-    0
+fn is_small(edge: &str) -> bool {
+    edge.chars().all(char::is_lowercase)
+}
+
+fn bfs_rec(
+    adjacency_list: &Input,
+    current: &str,
+    end: &str,
+    mut visited: HashMap<Edge, i32>,
+    visit: &dyn Fn(&Edge, &HashMap<Edge, i32>) -> bool,
+) -> i32 {
+    let mut paths = 0;
+    if current == end {
+        return 1;
+    }
+    if is_small(current) {
+        *visited.entry(current.to_string()).or_insert(0) += 1;
+    }
+    for neighbor in adjacency_list.get(current).unwrap() {
+        if visit(neighbor, &visited) {
+            paths += bfs_rec(adjacency_list, neighbor, end, visited.clone(), visit);
+        }
+    }
+    paths
+}
+
+pub fn solve_part1(input: &Input) -> i32 {
+    bfs_rec(input, "start", "end", HashMap::new(), &|edge, visited| {
+        !visited.contains_key(edge)
+    })
 }
 
 pub fn solve_part2(input: &Input) -> i32 {
-    0
+    bfs_rec(input, "start", "end", HashMap::new(), &|edge, visited| {
+        !visited.contains_key(edge)
+            || (visited.values().max().map_or(true, |max| *max == 1)
+                && edge != "start"
+                && edge != "end")
+    })
 }
 
 #[cfg(test)]
@@ -29,15 +87,23 @@ mod tests {
     #[test]
     fn example1() {
         let input = parse_input("src/problems_2021/problem12/example.txt");
-        assert_eq!(solve_part1(&input), 1656);
+        assert_eq!(solve_part1(&input), 10);
+        assert_eq!(solve_part2(&input), 36);
+    }
+
+    #[test]
+    fn example2() {
+        let input = parse_input("src/problems_2021/problem12/example2.txt");
+        assert_eq!(solve_part1(&input), 19);
+        assert_eq!(solve_part2(&input), 103);
     }
     #[test]
     fn part1() {
-        assert_eq!(solve_part1(&parse_input(INPUT_PATH)), 0);
+        assert_eq!(solve_part1(&parse_input(INPUT_PATH)), 4186);
     }
 
     #[test]
     fn part2() {
-        assert_eq!(solve_part2(&parse_input(INPUT_PATH)), 0);
+        assert_eq!(solve_part2(&parse_input(INPUT_PATH)), 92111);
     }
 }
