989bd36915b68d87ba0cfd5f23473f9f4e92bd23
Philipp Jung <philipp.jung@mailbox.org>
2021-12-18 10:46:49 +0100

    WIP

--- src/problems_2021/problem18.rs
+++ src/problems_2021/problem18.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, panic};
+use std::{collections::VecDeque, ops::Add, panic};
 
 use itertools::Itertools;
 use log::info;
@@ -19,10 +19,105 @@ pub fn solve() {
     );
 }
 
-type Input = Vec<bool>;
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum Number {
+    Pair(Box<Pair>),
+    Number(i32),
+}
+
+impl Number {
+    fn split(self) -> Option<Number> {
+        if let Number::Number(number) = self {
+            if number >= 10 {
+                return Some(Number::Pair(Box::new(Pair::new(
+                    Number::Number(number / 2),
+                    Number::Number(number / 2),
+                ))));
+            } else {
+                return None;
+            }
+        }
+        None
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Pair {
+    left: Number,
+    right: Number,
+}
+impl Add for Pair {
+    type Output = Pair;
+    fn add(self, other: Pair) -> Self::Output {
+        Pair {
+            left: Number::Pair(Box::new(self)),
+            right: Number::Pair(Box::new(other)),
+        }
+    }
+}
+
+impl Pair {
+    fn reduce(mut self, nested: i32) {
+        if let Number::Pair(left) = self.left {
+            left.reduce(nested + 1);
+        }
+        if let Number::Pair(right) = self.right {
+            right.reduce(nested + 1);
+        }
+        loop {
+            if nested >= 4 {
+                //self.explode();
+                continue;
+            } else if let Some(Number) = self.left.split() {
+                self.left = Number;
+            } else if let Some(Number) = self.right.split() {
+                self.right = Number;
+            } else {
+                return;
+            }
+        }
+    }
+
+    fn new(left: Number, right: Number) -> Pair {
+        Pair { left, right }
+    }
+}
+
+type Input = Vec<Pair>;
+
+fn parse_pair(input: &mut dyn Iterator<Item = char>) -> Pair {
+    let mut pair = Pair::new(Number::Number(0), Number::Number(0));
+    let mut active = &mut pair.left;
+    let mut open_processed = false;
+    while let Some(c) = input.next() {
+        match c {
+            '[' => {
+                if open_processed {
+                    *active = Number::Pair(Box::new(parse_pair(input)));
+                } else {
+                    open_processed = true;
+                }
+            }
+            ']' => break,
+            '0'..='9' => {
+                *active = Number::Number(c.to_digit(10).unwrap() as i32);
+            }
+            ',' => {
+                active = &mut pair.right;
+            }
+            _ => {
+                panic!("Unknown pattern {}", c)
+            }
+        }
+    }
+    pair
+}
 
 pub fn parse(input: &str) -> Input {
-    vec![]
+    input
+        .lines()
+        .map(|line| parse_pair(&mut line.chars()))
+        .collect_vec()
 }
 
 pub fn solve_part1(input: Input) -> u64 {
@@ -39,7 +134,17 @@ mod tests {
     use test_log::test;
 
     #[test]
-    fn example1() {}
+    fn example1() {
+        let first_pair = Pair::new(Number::Number(1), Number::Number(2));
+        assert_eq!(parse_pair(&mut "[1,2]".chars()), first_pair);
+        let second_pair = Pair::new(Number::Pair(Box::new(first_pair)), Number::Number(3));
+        assert_eq!(parse_pair(&mut "[[1,2],3]".chars()), second_pair);
+        let third_pair = Pair::new(
+            Number::Number(9),
+            Number::Pair(Box::new(Pair::new(Number::Number(8), Number::Number(7)))),
+        );
+        assert_eq!(parse_pair(&mut "[9,[8,7]]".chars()), third_pair);
+    }
 
     #[test]
     fn part1() {
