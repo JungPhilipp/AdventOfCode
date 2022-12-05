16b06778d0c8c3a98e4db5eca574e753adda84eb
Philipp Jung <philipp.jung@mailbox.org>
2022-12-05 09:21:15 +0100

    2022: Day5 Part1

--- src/problems_2022/day5.rs
+++ src/problems_2022/day5.rs
@@ -16,15 +16,65 @@ pub fn solve() {
         solve_part2(parsed)
     );
 }
-
-type Input = Vec<usize>;
+type Board = Vec<Vec<char>>;
+type Moves = Vec<(usize, usize)>;
+type Input = (Board, Moves);
 
 fn parse(input: &str) -> Input {
-    vec![]
+    let (board_input, moves_input) = input
+        .split("\n\n")
+        .filter(|line| !line.is_empty())
+        .collect_tuple()
+        .expect("Expected two parts");
+
+    let board = {
+        let mut stacks = Board::new();
+        stacks.resize(9, vec![]);
+        board_input
+            .split('\n')
+            .filter(|line| !line.is_empty() && !line.starts_with(" 1"))
+            .rev()
+            .for_each(|line| {
+                (0..9).for_each(|stack| {
+                    let pos = stack * 4 + 1;
+                    if let Some(c) = line.chars().nth(pos) {
+                        if c.is_ascii_uppercase() {
+                            stacks[stack].push(c)
+                        };
+                    }
+                });
+            });
+        stacks
+    };
+
+    let moves = {
+        moves_input
+            .split('\n')
+            .filter(|line| !line.is_empty())
+            .flat_map(|line| {
+                let (rep, from, to) = line
+                    .split(' ')
+                    .filter_map(|s| s.parse::<usize>().ok())
+                    .collect_tuple()
+                    .expect("Expected three numbers");
+                std::iter::repeat((from - 1, to - 1)).take(rep)
+            })
+            .collect_vec()
+    };
+
+    (board, moves)
 }
 
-fn solve_part1(input: Input) -> usize {
-    0
+fn solve_part1(input: Input) -> String {
+    let (mut board, moves) = input;
+    for (from, to) in moves {
+        let cargo = board[from].pop().expect("Stack should not be empty");
+        board[to].push(cargo);
+    }
+    board
+        .into_iter()
+        .filter_map(|mut stack| stack.pop())
+        .collect::<String>()
 }
 
 fn solve_part2(input: Input) -> usize {
@@ -38,12 +88,15 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert_eq!(solve_part1(parse(include_str!("day5/example_1.txt"))), 0);
+        assert_eq!(
+            solve_part1(parse(include_str!("day5/example_1.txt"))),
+            "CMZ"
+        );
     }
 
     #[test]
     fn part1() {
-        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 0);
+        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), "RNZLFZSJH");
     }
 
     #[test]
