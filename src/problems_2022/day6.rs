dcdbeb0b1ef3ecb35e87bd654ae65527652ceebb
Philipp Jung <philipp.jung@mailbox.org>
2022-12-06 08:42:55 +0100

    2022: Day6 Part 1

--- src/problems_2022/day6.rs
+++ src/problems_2022/day6.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 use log::info;
 
@@ -23,14 +25,32 @@ pub fn solve() {
     );
 }
 
-type Input = Vec<usize>;
+type Input = Vec<char>;
 
 fn parse(input: &str) -> Input {
-    vec![]
+    input.trim().chars().collect_vec()
 }
 
 fn solve_part1(input: Input) -> usize {
-    0
+    *input
+        .into_iter()
+        .enumerate()
+        .collect_vec()
+        .windows(4)
+        .find(|window| {
+            window
+                .iter()
+                .map(|(_, x)| x)
+                .cloned()
+                .collect::<HashSet<char>>()
+                .len()
+                == 4
+        })
+        .expect("Expected to find at least one")
+        .iter()
+        .map(|(index, _)| index)
+        .max()
+        .expect("Expected to find a max index") + 1
 }
 
 fn solve_part2(input: Input) -> usize {
@@ -44,12 +64,32 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert_eq!(solve_part1(parse(include_str!(EXAMPLE_1_PATH!()))), 0);
+        assert_eq!(solve_part1(parse("mjqjpqmgbljsphdztnvjfqwrcgsmlb")), 7);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(solve_part1(parse("bvwbjplbgvbhsrlpgdmjqwftvncz")), 5);
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(solve_part1(parse("nppdvjthqldpwncqszvftbrmjlhg")), 6);
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(solve_part1(parse("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")), 10);
+    }
+
+    #[test]
+    fn example_5() {
+        assert_eq!(solve_part1(parse("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")), 11);
     }
 
     #[test]
     fn part1() {
-        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 0);
+        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 1929);
     }
 
     #[test]
