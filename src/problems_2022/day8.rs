b380423e8a54f9fbe2dade6df609db0f7eb76c6e
Philipp Jung <philipp.jung@mailbox.org>
2022-12-08 07:52:53 +0100

    2022: Day 8 Part 1

--- src/problems_2022/day8.rs
+++ src/problems_2022/day8.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 use log::info;
+use ndarray::{iter::IterMut, Array, Array2, Axis};
 
 macro_rules! INPUT_PATH {
     () => {
@@ -19,14 +20,55 @@ pub fn solve() {
     );
 }
 
-type Input = Vec<char>;
+type Input = Array2<i32>;
 
 fn parse(input: &str) -> Input {
-    vec![]
+    let vec = input
+        .split('\n')
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).expect("Decimal number") as i32)
+                .collect_vec()
+        })
+        .collect_vec();
+    let x = vec[0].len();
+    let y = vec.len();
+    let flat_vec = vec.into_iter().flatten().collect_vec();
+    Array2::from_shape_vec((x, y), flat_vec).expect("regular matrix")
 }
 
-fn solve_part1(input: Input) -> usize {
-    0
+fn visible_trees<'a>(
+    trees: impl Iterator<Item = &'a i32>,
+    visibilities: impl Iterator<Item = &'a mut i32>,
+) {
+    let mut max = -1;
+    trees.zip(visibilities).for_each(|(&tree, visible)| {
+        if tree > max {
+            max = tree;
+            *visible += 1;
+        }
+    })
+}
+
+fn solve_part1(input: Input) -> i32 {
+    let mut flags: Input = Array::zeros(input.raw_dim());
+    input
+        .axis_iter(Axis(0))
+        .zip(flags.axis_iter_mut(Axis(0)))
+        .for_each(|(trees, mut visibilities)| {
+            visible_trees(trees.iter(), visibilities.iter_mut());
+            visible_trees(trees.iter().rev(), visibilities.iter_mut().rev());
+        });
+
+    input
+        .axis_iter(Axis(1))
+        .zip(flags.axis_iter_mut(Axis(1)))
+        .for_each(|(trees, mut visibilities)| {
+            visible_trees(trees.iter(), visibilities.iter_mut());
+            visible_trees(trees.iter().rev(), visibilities.iter_mut().rev());
+        });
+    flags.iter().map(|visible| (*visible > 0) as i32).sum()
 }
 
 fn solve_part2(input: Input) -> usize {
@@ -39,12 +81,17 @@ mod tests {
     use test_log::test;
 
     #[test]
+    fn example_1() {
+        assert_eq!(solve_part1(parse(include_str!("day8/example_1.txt"))), 21);
+    }
+
+    #[test]
     fn part1() {
-        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 0);
+        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))),1818 );
     }
 
     #[test]
     fn part2() {
-        assert_eq!(solve_part2(parse(include_str!(INPUT_PATH!()))), 0);
+        //assert_eq!(solve_part2(parse(include_str!(INPUT_PATH!()))), 0);
     }
 }
