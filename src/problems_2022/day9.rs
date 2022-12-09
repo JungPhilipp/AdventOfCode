66ee6186e1cefc4c575f04179f11ebb902fdd2cb
Philipp Jung <philipp.jung@mailbox.org>
2022-12-09 09:39:17 +0100

    2022: Day 9 Part 2

--- src/problems_2022/day9.rs
+++ src/problems_2022/day9.rs
@@ -2,17 +2,10 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 use log::info;
-use ndarray::{Array, Array2, Axis};
 
 macro_rules! INPUT_PATH {
     () => {
         "day9/input.txt"
-    };
-}
-
-macro_rules! EXAMPLE_PATH {
-    () => {
-        "day9/example_1.txt"
     };
 }
 
@@ -93,44 +86,52 @@ impl Point {
         let (x_dist, y_dist) = other.distance(self);
         self.x += x_dist.signum();
         self.y += y_dist.signum();
-
-        other.assert_in_range(self);
-    }
-
-    fn assert_in_range(&self, other: &Point) {
-        assert!(self.in_range(other), "{:?}, {:?}", self, other);
     }
 }
 
-fn do_step(head: &mut Point, tail: &mut Point, dir: Direction, steps: i32) -> Vec<Point> {
-    head.assert_in_range(tail);
+fn do_step(points: &mut [Point], dir: Direction, steps: i32) -> Vec<Point> {
     (0..steps)
         .map(|_| {
-            head.step(dir);
-            tail.catchup(head);
-            tail.clone()
+            points[0].step(dir);
+            for (i, j) in (0..points.len()).tuple_windows() {
+                let head = points[i].clone();
+                let tail = &mut points[j];
+                tail.catchup(&head);
+            }
+            points.last().unwrap().clone()
         })
         .collect()
 }
 
 fn solve_part1(input: Input) -> usize {
-    let mut head = Point::new(0, 0);
-    let mut tail = Point::new(0, 0);
+    let mut points = vec![Point::new(0, 0); 2];
     input
         .into_iter()
-        .flat_map(|(direction, steps)| do_step(&mut head, &mut tail, direction, steps))
+        .flat_map(|(direction, steps)| do_step(&mut points, direction, steps))
         .collect::<HashSet<Point>>()
         .len()
 }
 
-fn solve_part2(forest: Input) -> usize {
-    0
+fn solve_part2(input: Input) -> usize {
+    let mut points = vec![Point::new(0, 0); 10];
+    input
+        .into_iter()
+        .flat_map(|(direction, steps)| do_step(&mut points, direction, steps))
+        .collect::<HashSet<Point>>()
+        .into_iter()
+        .len()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use test_log::test;
+
+    macro_rules! EXAMPLE_PATH {
+        () => {
+            "day9/example_1.txt"
+        };
+    }
 
     #[test]
     fn example_1() {
@@ -144,11 +145,16 @@ mod tests {
 
     #[test]
     fn example_1_2() {
-        assert_eq!(solve_part2(parse(include_str!(EXAMPLE_PATH!()))), 0);
+        assert_eq!(solve_part2(parse(include_str!(EXAMPLE_PATH!()))), 1);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(solve_part2(parse(include_str!("day9/example_2.txt"))), 36);
     }
 
     #[test]
     fn part2() {
-        assert_eq!(solve_part2(parse(include_str!(INPUT_PATH!()))), 0);
+        assert_eq!(solve_part2(parse(include_str!(INPUT_PATH!()))), 2514);
     }
 }
