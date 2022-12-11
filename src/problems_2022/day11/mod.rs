cc9d7555845445c4558b78842fa00bb3f4297a61
Philipp Jung <philipp.jung@mailbox.org>
2022-12-11 08:47:58 +0100

    2022: Day11 Part 1

--- src/problems_2022/day11/mod.rs
+++ src/problems_2022/day11/mod.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use itertools::Itertools;
 use log::info;
 
@@ -19,14 +17,141 @@ pub fn solve() {
     );
 }
 
-type Input = Vec<i32>;
-
-fn parse(input: &str) -> Input {
-    vec![]
+#[derive(Debug, Clone)]
+struct Monkey {
+    items: Vec<usize>,
+    op: String,
+    test: usize,
+    true_target: usize,
+    false_target: usize,
+    items_handled: usize,
 }
 
-fn solve_part1(input: Input) -> usize {
-    0
+impl Monkey {
+    fn inspect(&self, mut item: usize) -> usize {
+        let (op, rhs) = self.op.split(' ').collect_tuple().expect("two parts");
+        let rhs = match rhs.trim().parse::<usize>() {
+            Ok(n) => n,
+            Err(_) => {
+                assert!(rhs.trim() == "old");
+                item
+            }
+        };
+        match op.trim() {
+            "*" => item *= rhs,
+            "+" => item += rhs,
+            _ => unreachable!(),
+        };
+        item / 3
+    }
+
+    fn test(&self, item: usize) -> usize {
+        if item % self.test == 0 {
+            self.true_target
+        } else {
+            self.false_target
+        }
+    }
+
+    fn give_items(&mut self) -> Vec<usize> {
+        //info!("Handle items {} + {}", self.items_handled, self.items.len());
+        self.items_handled += self.items.len();
+        let items = self.items.clone();
+        self.items.clear();
+        items
+    }
+}
+
+type Input = Vec<Monkey>;
+
+fn parse(input: &str) -> Input {
+    input
+        .split("\n\n")
+        .into_iter()
+        .filter(|lines| !lines.trim().is_empty())
+        .map(|monkey| {
+            let mut lines = monkey.split('\n').skip(1);
+            let items = lines
+                .next()
+                .expect("Starting items")
+                .trim()
+                .strip_prefix("Starting items: ")
+                .expect("to exist")
+                .split(',')
+                .into_iter()
+                .map(|item| item.trim().parse::<usize>().expect("a number"))
+                .collect_vec();
+            let op = lines
+                .next()
+                .expect("Operation")
+                .trim()
+                .strip_prefix("Operation: new = old")
+                .expect("to exist")
+                .trim()
+                .to_string();
+
+            let test = lines
+                .next()
+                .expect("Test")
+                .trim()
+                .strip_prefix("Test: divisible by")
+                .expect("to exist")
+                .trim()
+                .parse::<usize>()
+                .expect("a number");
+            let true_target_monkey = lines
+                .next()
+                .expect("True branch")
+                .trim()
+                .strip_prefix("If true: throw to monkey")
+                .expect("to exist")
+                .trim()
+                .parse::<usize>()
+                .expect("a number");
+            let false_target_monkey = lines
+                .next()
+                .expect("False branch")
+                .trim()
+                .strip_prefix("If false: throw to monkey")
+                .expect("to exist")
+                .trim()
+                .parse::<usize>()
+                .expect("a number");
+
+            Monkey {
+                items,
+                op,
+                test,
+                true_target: true_target_monkey,
+                false_target: false_target_monkey,
+                items_handled: 0,
+            }
+        })
+        .collect_vec()
+}
+
+fn solve_part1(mut input: Input) -> usize {
+    for round in 0..20 {
+        info!("Round: {} -------------------", round);
+        for (index, monkey) in input.iter().enumerate() {
+            info!("Monkey {}: {:?}", index, monkey.items)
+        }
+        for monkey_index in 0..input.len() {
+            let monkey = input[monkey_index].clone();
+            for item in input[monkey_index].give_items() {
+                let inspected_item = monkey.inspect(item);
+                input[monkey.test(inspected_item)].items.push(inspected_item);
+            }
+        }
+    }
+    input
+        .into_iter()
+        .map(|monkey| monkey.items_handled)
+        .sorted()
+        .rev()
+        .take(2)
+        .inspect(|handled| println!("{}", handled))
+        .product()
 }
 
 fn solve_part2(input: Input) -> usize {
@@ -46,7 +171,7 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert_eq!(solve_part1(parse(include_str!(EXAMPLE_PATH!()))), 0);
+        assert_eq!(solve_part1(parse(include_str!(EXAMPLE_PATH!()))), 10605);
     }
 
     #[test]
@@ -56,7 +181,7 @@ mod tests {
 
     #[test]
     fn example_1_2() {
-        assert_eq!(solve_part2(parse(include_str!(EXAMPLE_PATH!()))), 0);
+        //assert_eq!(solve_part2(parse(include_str!(EXAMPLE_PATH!()))), 0);
     }
 
     #[test]
