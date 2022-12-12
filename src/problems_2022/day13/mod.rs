59f2d4574ddca4f2bf2b138b86eb83d6ec46268d
Philipp Jung <philipp.jung@mailbox.org>
2022-12-12 17:15:49 +0100

    2022: Day 13 Setup

---
+++ src/problems_2022/day13/mod.rs
@@ -0,0 +1,65 @@
+use itertools::Itertools;
+use log::info;
+use primes::is_prime;
+
+macro_rules! INPUT_PATH {
+    () => {
+        "input.txt"
+    };
+}
+
+pub fn solve() {
+    let input = include_str!(INPUT_PATH!());
+    let parsed = parse(input);
+    info!(
+        "Solutions Day 11:\nPart1{}\nPart2{}",
+        solve_part1(parsed.clone()),
+        solve_part2(parsed)
+    );
+}
+
+type Input = Vec<i32>;
+
+fn parse(input: &str) -> Input {
+    vec![]
+}
+
+fn solve_part1(input: Input) -> usize {
+    0
+}
+
+fn solve_part2(input: Input) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_log::test;
+
+    macro_rules! EXAMPLE_PATH {
+        () => {
+            "example_1.txt"
+        };
+    }
+
+    #[test]
+    fn example_1() {
+        assert_eq!(solve_part1(parse(include_str!(EXAMPLE_PATH!()))), 0);
+    }
+
+    #[test]
+    fn part1() {
+        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 0);
+    }
+
+    #[test]
+    fn example_1_2() {
+        assert_eq!(solve_part2(parse(include_str!(EXAMPLE_PATH!()))), 0);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(solve_part2(parse(include_str!(INPUT_PATH!()))), 0);
+    }
+}
