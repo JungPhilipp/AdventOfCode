2224eea5e9ad6e96f7c0a5229b9dc1a32e48671f
Philipp Jung <philipp.jung@mailbox.org>
2022-12-13 12:07:38 +0100

    2022: Day 13 Part1 +2

--- src/problems_2022/day13/mod.rs
+++ src/problems_2022/day13/mod.rs
@@ -1,6 +1,5 @@
 use itertools::Itertools;
 use log::{debug, info};
-use primes::is_prime;
 
 macro_rules! INPUT_PATH {
     () => {
@@ -30,29 +29,27 @@ enum Op {
     Retry(Expand),
 }
 
-fn compare_char(lhs: char, rhs: char) -> Op {
+fn comp(lhs: &str, rhs: &str) -> Op {
     debug!("Compare: {} {}", lhs, rhs);
-    assert!(lhs.is_ascii_digit() || lhs == '[' || lhs == ']' || lhs == ',');
-    assert!(rhs.is_ascii_digit() || rhs == '[' || rhs == ']' || rhs == ',');
 
-    match (lhs.to_digit(10), rhs.to_digit(10)) {
+    match (lhs.parse::<usize>().ok(), rhs.parse::<usize>().ok()) {
         (None, None) => match (lhs, rhs) {
             _ if lhs == rhs => Op::Continue,
-            ('[', ']') => Op::Ordered(false),
-            (']', '[') => Op::Ordered(true),
-            (']', ',') => Op::Ordered(true),
-            (',', ']') => Op::Ordered(false),
+            ("[", "]") => Op::Ordered(false),
+            ("]", "[") => Op::Ordered(true),
+            ("]", ",") => Op::Ordered(true),
+            (",", "]") => Op::Ordered(false),
             _ => unreachable!("lhs:({}), rhs({})", lhs, rhs),
         },
         (None, Some(_)) => {
-            if lhs == '[' {
+            if lhs == "[" {
                 Op::Retry(Expand::Right)
             } else {
                 Op::Ordered(true)
             }
         }
         (Some(_), None) => {
-            if rhs == '[' {
+            if rhs == "[" {
                 Op::Retry(Expand::Left)
             } else {
                 Op::Ordered(false)
@@ -69,10 +66,34 @@ fn compare_char(lhs: char, rhs: char) -> Op {
 fn compare(lhs: &mut String, mut lhs_pos: usize, rhs: &mut String, mut rhs_pos: usize) -> bool {
     loop {
         debug!("Pair:\n{}\n{}", lhs, rhs);
-        let a = lhs.chars().nth(lhs_pos).expect("Should have next");
-        let b = rhs.chars().nth(rhs_pos).expect("Should have next");
+        let mut take_at_least_one = false;
+        let mut last_was_digit = false;
+        let a: String = lhs
+            .chars()
+            .skip(lhs_pos)
+            .take_while(|&c| {
+                let is_digit = c.is_ascii_digit();
+                let result = !take_at_least_one || (last_was_digit && is_digit);
+                last_was_digit = is_digit;
+                take_at_least_one = true;
+                result
+            })
+            .collect();
+        take_at_least_one = false;
+        last_was_digit = false;
+        let b: String = rhs
+            .chars()
+            .skip(rhs_pos)
+            .take_while(|&c| {
+                let is_digit = c.is_ascii_digit();
+                let result = !take_at_least_one || (last_was_digit && is_digit);
+                last_was_digit = is_digit;
+                take_at_least_one = true;
+                result
+            })
+            .collect();
 
-        match compare_char(a, b) {
+        match comp(a.as_str(), b.as_str()) {
             Op::Continue => {
                 lhs_pos += 1;
                 rhs_pos += 1;
@@ -80,11 +101,9 @@ fn compare(lhs: &mut String, mut lhs_pos: usize, rhs: &mut String, mut rhs_pos: 
             Op::Ordered(result) => return result,
             Op::Retry(side) => match side {
                 Expand::Left => {
-                    assert!(a.is_ascii_digit());
                     lhs.replace_range(lhs_pos..lhs_pos + 1, format!("[{}]", a).as_str())
                 }
                 Expand::Right => {
-                    assert!(b.is_ascii_digit());
                     rhs.replace_range(rhs_pos..rhs_pos + 1, format!("[{}]", b).as_str())
                 }
             },
@@ -92,8 +111,8 @@ fn compare(lhs: &mut String, mut lhs_pos: usize, rhs: &mut String, mut rhs_pos: 
     }
 }
 
-fn compare_packet(lhs: &mut String, rhs: &mut String) -> bool {
-    compare(lhs, 0, rhs, 0)
+fn compare_packet(mut lhs: String, mut rhs: String) -> bool {
+    compare(&mut lhs, 0, &mut rhs, 0)
 }
 
 fn parse(input: &str) -> Input {
@@ -115,17 +134,42 @@ fn solve_part1(input: Input) -> usize {
     input
         .into_iter()
         .enumerate()
-        .map(|(index, (mut lhs, mut rhs))| {
-            debug!("Pair: {}", index);
-            (index, compare_packet(&mut lhs, &mut rhs))
+        .map(|(index, (lhs, rhs))| {
+            debug!("Pair: {}", index + 1);
+            (index, compare_packet(lhs, rhs))
         })
-        .filter_map(|(index, valid)| if valid { Some(index) } else { None })
+        .filter_map(|(index, valid)| if valid { Some(index + 1) } else { None })
         .inspect(|index| info!("Valid pair at {}", index))
         .sum()
 }
 
+type Input2 = Vec<String>;
+const div_1: &str = "[[2]]";
+const div_2: &str = "[[6]]";
+
+fn parse_part2(input: Input) -> Input2 {
+    input
+        .into_iter()
+        .flat_map(|(first, second)| [first, second])
+        .chain([div_1, div_2].into_iter().map(|i| i.to_string()))
+        .collect_vec()
+}
 fn solve_part2(input: Input) -> usize {
-    0
+    parse_part2(input)
+        .into_iter()
+        .sorted_by(|lhs, rhs| match compare_packet(lhs.clone(), rhs.clone()) {
+            true => std::cmp::Ordering::Less,
+            false => std::cmp::Ordering::Greater,
+        })
+        .enumerate()
+        .filter_map(|(index, e)| {
+            if [div_1, div_2].contains(&e.as_str()) {
+                Some(index + 1)
+            } else {
+                None
+            }
+        })
+        .product()
 }
 
 #[cfg(test)]
@@ -141,75 +185,63 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert!(compare_packet(&mut "1".to_string(), &mut "2".to_string()));
-        assert!(!compare_packet(&mut "2".to_string(), &mut "1".to_string()));
+        assert!(compare_packet("1".to_string(), "2".to_string()));
+        assert!(!compare_packet("2".to_string(), "1".to_string()));
         assert!(compare_packet(
-            &mut "[1,1,3,1,1]".to_string(),
-            &mut "[1,1,5,1,1]".to_string()
-        ));
-        assert!(compare_packet(
-            &mut "[[4,4],4,4]".to_string(),
-            &mut "[[4,4],4,4,4]".to_string()
-        ));
-        assert!(!compare_packet(
-            &mut "[7,7,7,7]".to_string(),
-            &mut "[7,7,7]".to_string()
+            "[1,1,3,1,1]".to_string(),
+            "[1,1,5,1,1]".to_string()
         ));
         assert!(compare_packet(
-            &mut "[]".to_string(),
-            &mut "[3]".to_string()
+            "[[4,4],4,4]".to_string(),
+            "[[4,4],4,4,4]".to_string()
         ));
         assert!(!compare_packet(
-            &mut "[[[]]]".to_string(),
-            &mut "[[]]".to_string()
+            "[7,7,7,7]".to_string(),
+            "[7,7,7]".to_string()
         ));
+        assert!(compare_packet("[]".to_string(), "[3]".to_string()));
+        assert!(!compare_packet("[[[]]]".to_string(), "[[]]".to_string()));
         assert!(!compare_packet(
-            &mut "[1,[2,[3,[4,[5,6,7]]]],8,9]".to_string(),
-            &mut "[1,[2,[3,[4,[5,6,0]]]],8,9]".to_string()
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]".to_string(),
+            "[1,[2,[3,[4,[5,6,0]]]],8,9]".to_string()
         ));
 
-        assert!(!compare_packet(
-            &mut "[9]".to_string(),
-            &mut "[[8,7,6]]".to_string()
-        ));
+        assert!(!compare_packet("[9]".to_string(), "[[8,7,6]]".to_string()));
         assert!(compare_packet(
-            &mut "[[1],[2,3,4]]".to_string(),
-            &mut "[[1],4]".to_string()
+            "[[1],[2,3,4]]".to_string(),
+            "[[1],4]".to_string()
         ));
         assert_eq!(solve_part1(parse(include_str!(EXAMPLE_PATH!()))), 13);
     }
     #[test]
     fn example_1_custom() {
         assert!(compare_packet(
-            &mut "[[1],[2,3,4]]".to_string(),
-            &mut "[[1],4,5]".to_string()
+            "[[1],[2,3,4]]".to_string(),
+            "[[1],4,5]".to_string()
         ));
         assert!(!compare_packet(
-            &mut "[[8,6,[[9,0,0],2,[6,3,2,0]],[],6],[10,[10,10,8,2],[[4,3,7],4,8],1,7]]"
-                .to_string(),
-            &mut "[[[],0,[[8,8,10,5,7],1,5,0],[]]]".to_string()
+            "[[8,6,[[9,0,0],2,[6,3,2,0]],[],6],[10,[10,10,8,2],[[4,3,7],4,8],1,7]]".to_string(),
+            "[[[],0,[[8,8,10,5,7],1,5,0],[]]]".to_string()
         ));
         assert!(compare_packet(
-            &mut "[[[],6]]".to_string(),
-            &mut "[[9,[],3,[9]],[],[[1],[10,9,4,[]],[2]],[]]".to_string()
+            "[[[],6]]".to_string(),
+            "[[9,[],3,[9]],[],[[1],[10,9,4,[]],[2]],[]]".to_string()
         ));
-        assert!(compare_packet(
-            &mut "[[],[]]".to_string(),
-            &mut "[10]".to_string()
-        ));
+        assert!(compare_packet("[[],[]]".to_string(), "[10]".to_string()));
         assert!(!compare_packet(
-            &mut "[[10,1],[]]".to_string(),
-            &mut "[10]".to_string()
+            "[[10,1],[]]".to_string(),
+            "[10]".to_string()
         ));
-        assert!(compare_packet(
-            &mut "[8]".to_string(),
-            &mut "[[[8,7,6]]]".to_string()
+        assert!(compare_packet("[8]".to_string(), "[[[8,7,6]]]".to_string()));
+        assert!(!compare_packet(
+            "[[10,3],[3,1,[3],8,5],[],[6,2,7,[10],[[0,8,9,10],5]]]".to_string(),
+            "[[4],[0,1,[10]],[],[],[0,[[3],[7,7],8,[9,0,0],3],3,8]]".to_string()
         ));
     }
 
     #[test]
     fn part1() {
-        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 5232);
+        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 5252);
     }
 
     #[test]
