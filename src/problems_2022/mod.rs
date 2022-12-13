9488d889f9deb6eee9c214ba637dd84b94ab368e
Philipp Jung <philipp.jung@mailbox.org>
2022-12-13 12:13:41 +0100

    2022: Day 14 Setup

--- src/problems_2022/mod.rs
+++ src/problems_2022/mod.rs
@@ -3,6 +3,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 mod day2;
 mod day3;
 mod day4;
@@ -26,4 +27,5 @@ pub fn solve() {
     day11::solve();
     day12::solve();
     day13::solve();
+    day14::solve();
 }
