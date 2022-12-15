48f965fcf406457351f9cbc56e1db9c036bcf405
Philipp Jung <philipp.jung@mailbox.org>
2022-12-15 10:38:26 +0100

    2022: Day 15 Part 1

--- src/problems_2022/day15/mod.rs
+++ src/problems_2022/day15/mod.rs
@@ -1,10 +1,12 @@
 use std::{
     cmp::{max, min},
     collections::HashSet,
+    ops::Index,
 };
 
 use itertools::Itertools;
 use log::info;
+use regex::Regex;
 
 macro_rules! INPUT_PATH {
     () => {
@@ -17,22 +19,83 @@ pub fn solve() {
     let parsed = parse(input);
     info!(
         "Solutions Day 15:\nPart1{}\nPart2{}",
-        solve_part1(parsed.clone()),
-        solve_part2(parsed)
+        solve_part1(parsed.clone(), 2000000),
+        solve_part2(parsed, 0, 400000),
     );
 }
 
-type Input = Vec<i32>;
+type Point = (i32, i32);
+
+type Input = Vec<(Point, Point)>;
 
 fn parse(input: &str) -> Input {
-    vec![]
+    let expression =
+        Regex::new(r"Sensor at x=(-*\d+), y=(-*\d+): closest beacon is at x=(-*\d+), y=(-*\d+)")
+            .unwrap();
+    input
+        .split('\n')
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            expression
+                .captures_iter(line)
+                .map(|matches| {
+                    (1..5)
+                        .map(|i| {
+                            matches
+                                .get(i)
+                                .expect("Should contain four groups")
+                                .as_str()
+                                .parse::<i32>()
+                                .expect("Should be a number")
+                        })
+                        .tuples()
+                        .map(|(x, y)| (x, y))
+                        .collect_tuple::<(Point, Point)>()
+                        .unwrap()
+                })
+                .exactly_one()
+                .unwrap()
+        })
+        .collect()
 }
 
-fn solve_part1(input: Input) -> usize {
-    0
+fn manhattan_distance(lhs: &Point, rhs: &Point) -> usize {
+    ((lhs.0 - rhs.0).abs() + (lhs.1 - rhs.1).abs()) as usize
 }
 
-fn solve_part2(input: Input) -> usize {
+fn visible_for_sensor(sensor: &Point, beacon: &Point, point: &Point) -> bool {
+    let sensor_range = manhattan_distance(sensor, beacon);
+    let dist_to_point = manhattan_distance(sensor, point);
+    dist_to_point <= sensor_range
+}
+
+fn solve_part1(input: Input, row: i32) -> usize {
+    let max_x = 10_000_000;
+    let mut impossible_positions = 0;
+    for x in -max_x..=max_x {
+        let point = (x, row);
+        for (sensor, beacon) in input.iter() {
+            if point == *sensor || point == *beacon {
+                break;
+            }
+            if visible_for_sensor(sensor, beacon, &point) {
+                info!("{:?}", point);
+                impossible_positions += 1;
+                break;
+            }
+        }
+    }
+    impossible_positions
+}
+
+type range = (i32, i32);
+fn solve_part2(input: Input, min: i32, max: i32) -> usize {
+    let x_ranges = vec![];
+    let y_ranges = vec![];
+    let squares = input
+        .into_iter()
+        .map(|(sensor, beacon)| (sensor, manhattan_distance(&sensor, &beacon)))
+        .collect_vec();
     0
 }
 
@@ -50,21 +113,30 @@ mod tests {
     #[test]
     fn example_1() {
         let input = parse(include_str!(EXAMPLE_PATH!()));
-        assert_eq!(solve_part1(input), 0);
+        assert_eq!(solve_part1(input, 10), 26);
     }
 
     #[test]
     fn part1() {
-        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 0);
+        assert_eq!(
+            solve_part1(parse(include_str!(INPUT_PATH!())), 2000000),
+            5142231
+        );
     }
 
     #[test]
     fn example_1_2() {
-        assert_eq!(solve_part2(parse(include_str!(EXAMPLE_PATH!()))), 0);
+        assert_eq!(
+            solve_part2(parse(include_str!(EXAMPLE_PATH!())), 0, 20),
+            56000011
+        );
     }
 
     #[test]
     fn part2() {
-        assert_eq!(solve_part2(parse(include_str!(INPUT_PATH!()))), 0);
+        assert_eq!(
+            solve_part2(parse(include_str!(INPUT_PATH!())), 0, 4000000),
+            0
+        );
     }
 }
--- src/problems_2022/mod.rs
+++ src/problems_2022/mod.rs
@@ -4,6 +4,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 mod day2;
 mod day3;
 mod day4;
@@ -28,4 +29,5 @@ pub fn solve() {
     day12::solve();
     day13::solve();
     day14::solve();
+    day15::solve();
 }
