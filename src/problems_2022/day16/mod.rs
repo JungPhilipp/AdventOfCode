15c9a128bc60fdf279910fcf468851308feea7eb
Philipp Jung <philipp.jung@mailbox.org>
2022-12-16 17:16:51 +0100

    2022: Day 16 Part 1 + Part 2 (Bruteforce)

--- src/problems_2022/day16/mod.rs
+++ src/problems_2022/day16/mod.rs
@@ -1,10 +1,11 @@
 use std::{
     cmp::{max, min},
-    collections::HashSet,
+    collections::{BTreeSet, HashMap, HashSet, VecDeque},
 };
 
 use itertools::Itertools;
 use log::info;
+use regex::Regex;
 
 macro_rules! INPUT_PATH {
     () => {
@@ -21,19 +22,180 @@ pub fn solve() {
         solve_part2(parsed)
     );
 }
-
-type Input = Vec<i32>;
+type Input = HashMap<Room, (Flow, Vec<Room>)>;
 
 fn parse(input: &str) -> Input {
-    vec![]
+    let expression =
+        Regex::new(r"Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnels? leads? to valves? (.*)")
+            .unwrap();
+    expression
+        .captures_iter(input)
+        .map(|line| {
+            let name = to_name(&line[1]);
+            let flow = line[2].parse::<Flow>().expect("Flow should be integer");
+            let neighbors = line[3]
+                .split(',')
+                .map(|name| to_name(name.trim()))
+                .collect_vec();
+            (name, (flow, neighbors))
+        })
+        .collect()
+}
+type Cost = usize;
+type Graph = HashMap<Room, (Flow, HashMap<Room, Cost>)>;
+
+fn cleanup_graph(graph: Input) -> Graph {
+    let mut graph_with_costs = graph
+        .into_iter()
+        .map(|(key, value)| {
+            let new_value = (
+                value.0,
+                value.1.into_iter().map(|neighbor| (neighbor, 1)).collect(),
+            );
+            (key, new_value)
+        })
+        .collect::<Graph>();
+
+    while let Some((room, (_, neighbors))) = graph_with_costs
+        .iter()
+        .find(|(_, value)| value.0 == 0)
+        .map(|(key, value)| (*key, value.clone()))
+    {
+        for (neighbor, costs) in neighbors.iter() {
+            for (other_neighbor, other_costs) in neighbors.iter() {
+                if let Some((_, neighbors)) = graph_with_costs.get_mut(neighbor) {
+                    let new_costs = neighbors.entry(*other_neighbor).or_insert(Cost::MAX);
+                    *new_costs = min(*new_costs, *costs + *other_costs);
+                }
+            }
+        }
+        graph_with_costs.remove(&room);
+    }
+
+    graph_with_costs
+}
+
+type Flow = usize;
+
+fn to_name(name: &str) -> Room {
+    name.chars().collect_tuple().expect("Should be two chars")
+}
+type Room = (char, char);
+type Time = i64;
+
+fn max_flow(
+    graph: &HashMap<Room, (Flow, Vec<Room>)>,
+    room: Room,
+    time: Time,
+    opened: &Vec<Room>,
+    cache: &mut HashMap<(Room, Time, Vec<Room>), Flow>,
+) -> usize {
+    if time <= 0 || opened.len() == graph.len() {
+        return 0;
+    }
+    let cache_key = (room, time, opened.clone());
+    if let Some(flow) = cache.get(&cache_key) {
+        return *flow;
+    }
+    let (flow_rate, neighbors) = graph.get(&room).unwrap();
+
+    let without_open = neighbors
+        .iter()
+        .map(|neighbor| max_flow(graph, *neighbor, time - 1, opened, cache))
+        .max()
+        .unwrap();
+
+    let next_opened = opened
+        .iter()
+        .cloned()
+        .chain([room].into_iter())
+        .sorted()
+        .collect_vec();
+
+    let with_open = if *flow_rate > 0 && !opened.contains(&room) {
+        (time - 1) as usize * flow_rate + max_flow(graph, room, time - 1, &next_opened, cache)
+    } else {
+        0
+    };
+
+    let flow = max(without_open, with_open);
+    cache.insert(cache_key, flow);
+    flow
 }
 
 fn solve_part1(input: Input) -> usize {
-    0
+    max_flow(&input, ('A', 'A'), 30, &vec![], &mut HashMap::new())
+}
+
+fn max_flow_vec(
+    graph: &Graph,
+    rooms: (Room, Room),
+    time: Time,
+    opened: &Vec<Room>,
+    cache: &mut HashMap<((Room, Room), Time, Vec<Room>), Flow>,
+) -> usize {
+    if time <= 0 || opened.len() == graph.len() {
+        return 0;
+    }
+    let cache_key = (rooms, time, opened.clone());
+    if let Some(flow) = cache.get(&cache_key) {
+        return *flow;
+    }
+
+    let mut flow = 0;
+    let (flow_rate_1, neighbors_1) = graph.get(&rooms.0).unwrap();
+    let (flow_rate_2, neighbors_2) = graph.get(&rooms.1).unwrap();
+
+    for (neighbor_1, costs_1) in neighbors_1.iter()
+    /*.chain([(rooms.0, 1)].iter()) */
+    {
+        if *neighbor_1 == rooms.0 && (*flow_rate_1 == 0 || opened.contains(&rooms.0)) {
+            continue;
+        }
+
+        for (neighbor_2, costs_2) in neighbors_2.iter()
+        /*.chain([rooms.1].iter()) */
+        {
+            let mut flow_inner = 0;
+            let mut opened_new = opened.clone();
+            if *neighbor_1 == rooms.0 {
+                opened_new.push(rooms.0);
+                flow_inner += (time - 1) as usize * flow_rate_1;
+            }
+            if *neighbor_2 == rooms.1 {
+                if *flow_rate_2 == 0 || opened_new.contains(&rooms.1) {
+                    continue;
+                }
+                opened_new.push(rooms.1);
+                flow_inner += (time - 1) as usize * flow_rate_2;
+            }
+            flow_inner += max_flow_vec(
+                graph,
+                [*neighbor_1, *neighbor_2]
+                    .into_iter()
+                    .sorted()
+                    .collect_tuple()
+                    .unwrap(),
+                time - 1,
+                &opened_new,
+                cache,
+            );
+            flow = max(flow, flow_inner);
+        }
+    }
+
+    cache.insert(cache_key, flow);
+    flow
 }
 
 fn solve_part2(input: Input) -> usize {
-    0
+    max_flow_vec(
+        &cleanup_graph(input),
+        (('A', 'A'), ('A', 'A')),
+        26,
+        &vec![],
+        &mut HashMap::new(),
+    )
 }
 
 #[cfg(test)]
@@ -50,17 +212,17 @@ mod tests {
     #[test]
     fn example_1() {
         let input = parse(include_str!(EXAMPLE_PATH!()));
-        assert_eq!(solve_part1(input), 0);
+        assert_eq!(solve_part1(input), 1651);
     }
 
     #[test]
     fn part1() {
-        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 0);
+        assert_eq!(solve_part1(parse(include_str!(INPUT_PATH!()))), 2330);
     }
 
     #[test]
     fn example_1_2() {
-        assert_eq!(solve_part2(parse(include_str!(EXAMPLE_PATH!()))), 0);
+        assert_eq!(solve_part2(parse(include_str!(EXAMPLE_PATH!()))), 1707);
     }
 
     #[test]
